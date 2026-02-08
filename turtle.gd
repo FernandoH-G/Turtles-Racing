8e4e2fe27cd39c9c2cff3e975f97237dcb982cf8
Fernando H-G <fherrera2018@gmail.com>
2026-02-07 19:45:42 -0600

    Have the top turtle moving from point a to point b.

--- turtle.gd
+++ turtle.gd
@@ -1,15 +1,28 @@
-extends Node
+extends CharacterBody2D
 
-enum State {
-	IDLE,
-	FOLLOW,
-}
+const SPEED = 100.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var target = Vector2(1000,0)
 
+func _physics_process(_delta: float) -> void:
+	velocity = position.direction_to(target) * SPEED
+	if position.distance_to(target) > 10:
+		move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+	# # Add the gravity.
+	# if not is_on_floor():
+	# 	velocity += get_gravity() * delta
+
+	# # Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 	velocity.y = JUMP_VELOCITY
+
+	# # Get the input direction and handle the movement/deceleration.
+	# # As good practice, you should replace UI actions with custom gameplay actions.
+	# var direction := Input.get_axis("ui_left", "ui_right")
+	# if direction:
+	# 	velocity.x = direction * SPEED
+	# else:
+	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# move_and_slide()
