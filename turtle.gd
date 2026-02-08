115af4a3c5fd7f74457e29196601ed0f52d1ec25
Fernando H-G <fherrera2018@gmail.com>
2026-02-07 19:45:42 -0600

    Updated turtle speed and added potential code to enable userBoost to the turtles speed.

--- turtle.gd
+++ turtle.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-
 var target = Vector2(1000,0)
 
 func _physics_process(_delta: float) -> void:
-	velocity = position.direction_to(target) * SPEED
+	# userBoost will be enabled (and timed out if not reclicked) by the user to
+	# give their turtle a little boost. Will be the min speed value.
+	# var userBoost = randi_range(10, 20)
+
+	var speed =  randi_range(0, 200)
+	# print(speed)
+	velocity = position.direction_to(target) * speed
 	if position.distance_to(target) > 10:
 		move_and_slide()
 
