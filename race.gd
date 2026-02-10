226058cf901d44043de5caf7e14fb5ef6d4972ef
Fernando H-G <fherrera2018@gmail.com>
2026-02-09 22:32:30 -0600

    Added logic so that restart race and exit buttons work as intended. Adjusted winner_particles to not use as much height.

--- race.gd
+++ race.gd
@@ -6,6 +6,8 @@ var have_winner = false
 func _ready() -> void:
 	$WinnerParticles.hide()
 	$VBoxContainer.hide()
+	$VBoxContainer/ResetButton.hide()
+	$VBoxContainer/ExitButton.hide()
 
 
 func _on_turtle_v_1_finished_race(turtle_name: String) -> void:
@@ -23,4 +25,15 @@ func _on_turtle_v_2_finished_race(turtle_name: String) -> void:
 func _on_declare_winner(winner_name: String) -> void:
 	$WinnerParticles.show()
 	$VBoxContainer.show()
+	$VBoxContainer/ResetButton.show()
+	$VBoxContainer/ExitButton.show()
 	$WinnerLabel.text = "%s is the winner!" % winner_name
+
+
+func _on_exit_button_pressed() -> void:
+	print("pressed exit button")
+	get_tree().quit()
+
+
+func _on_reset_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/countdown.tscn")
