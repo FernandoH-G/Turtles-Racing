aee38ee4cc5e93b5fbc07662d683403768248920
Fernando H-G <fherrera2018@gmail.com>
2026-02-17 00:45:45 -0600

    Created turtle_f
    turtle_f consists of three components:
    1. AnimatedSprite
    2. DirtParticle
    3. Turtle movement script
    
    turtle_f also has a gd script that handles finished race logic.

--- turtle_f.gd
+++ turtle_f.gd
@@ -3,8 +3,13 @@ class_name Turtle extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var dust_particle: GPUParticles2D = $DustParticle
+@export var turtle_movement_c: TurtleMovementC
 
 
-func _on_turtle_movement_c_finished_race(turtle_name: String) -> void:
+func _ready() -> void:
+	turtle_movement_c.finished_race.connect(finish_race)
+
+
+func finish_race(turtle_name: String)->void:
 	animated_sprite.stop()
 	dust_particle.emitting = false
--- turtle_movement_c.gd
+++ turtle_movement_c.gd
@@ -2,13 +2,11 @@ class_name TurtleMovementC extends Node
 
 
 signal finished_race(turtle_name: String)
-@export var end_point: Marker2D = null
 @export var min_speed: int = 0
 @export var max_speed: int = 200
 @export var turtle_name: String = "Fern"
 @onready var parent: CharacterBody2D = get_parent()
-
-
+var end_point: Marker2D = null
 var end_position: Vector2
 
 
@@ -17,18 +15,27 @@ func _ready() -> void:
 	var start_position: Vector2
 	start_position = parent.position
 	if end_point == null:
-		end_position = start_position + Vector2(1000, 0)
+		end_position = start_position + Vector2(200, 0)
 		return
 	end_position = end_point.global_position 
+
+
+func move_forward() -> void:
+	var speed = randi_range(min_speed, max_speed)
+	parent.velocity = parent.position.direction_to(end_position) * speed
+	parent.move_and_slide()
+	
+	
+func stop_moving() -> void:
+	finished_race.emit(turtle_name)
 	
 	
 func _physics_process(_delta: float) -> void:
 	# userBoost will be enabled (and timed out if not reclicked) by the user to
 	# give their turtle a little boost. Will be the min speed value.
 	# var userBoost = randi_range(10, 20)
-	var speed = randi_range(min_speed, max_speed)
-	parent.velocity = parent.position.direction_to(end_position) * speed
+	
 	if parent.position.distance_to(end_position) > 10:
-		parent.move_and_slide()
+		move_forward()
 	else:
-		finished_race.emit(turtle_name)
+		stop_moving()
